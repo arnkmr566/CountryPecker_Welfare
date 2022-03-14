4088a88ec2e41d21700b6beba1a8e847446a10f0
ARUN KUMAR <arnkmr566@gmail.com>
2022-03-14 08:40:38 +0530

    CountryPecker_Welfare

---
+++ app/src/main/java/com/example/countrypecker/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.countrypecker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.rilixtech.widget.countrycodepicker.Country;
+import com.rilixtech.widget.countrycodepicker.CountryCodePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    CountryCodePicker ccp;  //  Country code picker search
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ccp = findViewById(R.id.ccp);
+
+        ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected(Country selectedCountry) {
+                Toast.makeText(getApplicationContext(), "Updated " + selectedCountry.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
